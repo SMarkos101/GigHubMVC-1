625c5058739c3e8ebf94005691b1fd135d4ad8c2
Panos Bozas <fbozas@hotmail.com>
2021-10-25 19:45:35 +0300

    Implementing Validation

---
+++ GigHubMVC/CustomAnnotations/ValidTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHubMVC.CustomAnnotations
+{
+    public class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime time;
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "HH:mm",
+                DateTimeFormatInfo.InvariantInfo,
+                DateTimeStyles.None, out time);
+
+            return (isValid);
+        }
+    }
+}
